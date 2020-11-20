f7f6ff234f3abd90c1e51ea2680a805a8117823d
AGuyNextDoor <martin.vielvoye@gmail.com>
2020-11-20 18:21:11 +0100

    conflict

--- Martin/js_algo_2/index.js
+++ Martin/js_algo_2/index.js
@@ -1,37 +1,31 @@
-const taille = require("./taille");
-const clean = require("./clean");
+const taille = require("./taille")
+const clean = require("./clean")
 
-function animalCount(dataList) {
+
+function animalCount(dataList){
   let result = 0;
   let cleanedData = dataList.map(clean)
-
+  
   for (let index = 0; index < cleanedData.length; index++) {
     result = result + taille(cleanedData[index]);
   }
-
-  return result;
+  
+  return result
+  
 }
+
 // email = "azepoaziepoaizepo".split().map()
 // if(email)
 
-const data = [
-  ["Arthus", "Mathis", "Juliette"],
-  ["Bob", "Prince"],
-  [""],
-  ["Nesta"],
-  ["Gandhi"],
-  [""],
-  ["Neige"],
-];
+const data = [["Arthus", "Mathis", "Juliette"], ["Bob", "Prince"], [""], ["Nesta"], ["Gandhi"], [""], ["Neige"]]
 //           [                 0               ,        1         , 2,      3
 const tailleData = animalCount(data) // 8
 console.log(tailleData)
-/*
-=======
+/* 
 
-Par exemple:
+Par exemple: 
 
 let result = animalCount(data)
 console.log(result) // => 8
 
-*/
+*/ 
